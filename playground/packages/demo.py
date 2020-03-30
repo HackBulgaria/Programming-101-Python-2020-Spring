254bed9210ebd9a235ad6eadc178445957861dbb
martin056 <martinangelov056@gmail.com>
2020-03-30 17:25:31 +0300

    Show sys.path and import foo as module

---
+++ playground/packages/demo.py
@@ -0,0 +1,11 @@
+import sys
+import random
+
+import foo
+
+print(sys.path)
+
+foo.func(10)
+
+panda = foo.Panda()
+print(panda)
---
+++ playground/packages/foo.py
@@ -0,0 +1,6 @@
+def func(x):
+    print(f'Passed arg: {x}')
+
+
+class Panda:
+    pass
