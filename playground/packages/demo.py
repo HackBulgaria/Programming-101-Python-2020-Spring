a5fa5192879005aea9808ef76dedb6f464cdfa6d
martin056 <martinangelov056@gmail.com>
2020-03-30 17:40:22 +0300

    Show how to use `as` keyword

--- playground/packages/demo.py
+++ playground/packages/demo.py
@@ -1,15 +1,19 @@
 import sys
 from random import shuffle
 
-from foo import *
+def func(x):
+    print('You will not see me')
+
+from foo import func as func_new_name
 
 print('Namespace after imports: ')
 print(dir())
 print('==========================')
 
 func(10)
+func_new_name(10)
 
-panda = foo.Panda()
-print(panda)
+# panda = foo.Panda()
+# print(panda)
 
-print('Mega constant: ', foo._MEGA_CONSTANT)
+# print('Mega constant: ', foo._MEGA_CONSTANT)
